4de8ff519ede10689cb5c25f382332c3461eb93b
selenyX <yigexie@usc.edu>
2023-10-02 02:13:25 -0700

    level 2 puzzle complete
    need win state and scene transition

--- Prototype_Fluid/Assets/Scripts/ReverseGravity.cs
+++ Prototype_Fluid/Assets/Scripts/ReverseGravity.cs
@@ -6,6 +6,7 @@ public class ReverseGravity : MonoBehaviour
 {
     private Rigidbody rb;
     private bool reverseGravity = false;
+    [SerializeField] float gravityIntensity = -10;
 
     private void Start()
     {
@@ -33,7 +34,7 @@ public class ReverseGravity : MonoBehaviour
             {
                 rb.useGravity = false; // Disable the normal gravity.
                 rb.velocity = Vector3.zero; // Reset velocity.
-                rb.AddForce(Physics.gravity * -1, ForceMode.Acceleration); // Apply reverse gravity.
+                rb.AddForce(Physics.gravity * gravityIntensity, ForceMode.Acceleration); // Apply reverse gravity.
             }
             else
             {
