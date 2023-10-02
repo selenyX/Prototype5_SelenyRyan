f550ab85d3fdd2dd9592ac877f0a99b8de949b87
selenyX <yigexie@usc.edu>
2023-10-02 01:12:48 -0700

    add in level2

---
+++ Prototype_Fluid/Assets/Scripts/ReverseGravity.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseGravity : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool reverseGravity = false;
+
+    private void Start()
+    {
+        // Get the Rigidbody component attached to the GameObject.
+        rb = GetComponent<Rigidbody>();
+
+        // Ensure that the Rigidbody component exists.
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component not found on the GameObject.");
+            enabled = false; // Disable the script if Rigidbody is missing.
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("NoGravity")) 
+        {
+            // Reverse the gravity direction.
+            reverseGravity = !reverseGravity;
+
+            // Apply the reverse gravity effect.
+            if (reverseGravity)
+            {
+                rb.useGravity = false; // Disable the normal gravity.
+                rb.velocity = Vector3.zero; // Reset velocity.
+                rb.AddForce(Physics.gravity * -1, ForceMode.Acceleration); // Apply reverse gravity.
+            }
+            else
+            {
+                rb.useGravity = true; // Enable normal gravity.
+            }
+        }
+    }
+}
