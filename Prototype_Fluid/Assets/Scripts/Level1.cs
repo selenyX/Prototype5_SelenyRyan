040104a35a5d15a2297f1ea499ceb34a282ec246
Ryan Lau <rclau@usc.edu>
2023-10-02 19:27:21 -0700

    win state and transition

--- Prototype_Fluid/Assets/Scripts/Level1.cs
+++ Prototype_Fluid/Assets/Scripts/Level1.cs
@@ -2,21 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Level1 : MonoBehaviour
 {
+    public Target LeftTarget;
+    public Target RightTarget;
+    public TMP_Text victoryText;
+    float endTimer = 0.0f;
+    bool complete = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        victoryText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R)) // retry
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        } 
+        }
+        if (!complete && LeftTarget.GetCount() >= 300 && RightTarget.GetCount() >= 300) // level complete
+        {
+            complete = true;
+            victoryText.gameObject.SetActive(true);
+            endTimer = 1.0f;
+        }
+        if (endTimer > 0.0f) // wait one second before moving to next level
+        {
+            endTimer -= Time.deltaTime;
+            if (endTimer <= 0.0f)
+            {
+                SceneManager.LoadScene("Scene02");
+            }
+        }
     }
 }
--- Prototype_Fluid/Assets/Scripts/Level2.cs
+++ Prototype_Fluid/Assets/Scripts/Level2.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Level2 : MonoBehaviour
 {
+    public Target Target;
+    public TMP_Text victoryText;
+    float endTimer = 0.0f;
+    bool complete = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        victoryText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +23,24 @@ public class Level2 : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log(Target.GetCount());
+        }
+        if (!complete && Target.GetCount() >= 600) // level complete
+        {
+            complete = true;
+            victoryText.gameObject.SetActive(true);
+            endTimer = 1.0f;
+        }
+        if (endTimer > 0.0f)
+        {
+            endTimer -= Time.deltaTime;
+            if (endTimer <= 0.0f)
+            {
+                SceneManager.LoadScene("Scene01");
+            }
         }
     }
 }
