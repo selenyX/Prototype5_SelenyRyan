3ebfaa33f2904ee41fdb61f9036a38998c2bc5be
Ryan Lau <rclau@usc.edu>
2023-10-02 20:36:19 -0700

    level 2 up and down pipe movement

---
+++ Prototype_Fluid/Assets/Scripts/Pipe2.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe2 : MonoBehaviour
+{
+    Renderer rend;
+    private Color startcolor;
+    bool selected = false;
+    Vector3 initialPos;
+    float maxHeight;
+    float minHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        initialPos = transform.position;
+        maxHeight = initialPos.y + (transform.localScale.y / 2);
+        minHeight = initialPos.y - (transform.localScale.y / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                transform.position += new Vector3(0, 2 * Time.deltaTime, 0);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                transform.position += new Vector3(0, -2 * Time.deltaTime, 0);
+            }
+            
+            if (transform.position.y > maxHeight)
+            {
+                transform.position = new Vector3(initialPos.x, maxHeight, initialPos.z);
+            }
+            if (transform.position.y < minHeight)
+            {
+                transform.position = new Vector3(initialPos.x, minHeight, initialPos.z);
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (selected) // deselect
+        {
+            rend.material.color = startcolor;
+            selected = false;
+        } 
+        else // select
+        {
+            startcolor = GetComponent<Renderer>().material.color;
+            rend.material.color = Color.green;
+            selected = true;
+        }    
+    }
+}
